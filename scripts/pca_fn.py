06ce545821603481b8ec42e264e7c69a42de7dcf
Pierre Sempere <pierre.sempere.01@gmail.com>
2022-05-03 19:46:57 +0200

    PCA

---
+++ scripts/pca_fn.py
@@ -0,0 +1,66 @@
+import matplotlib.pyplot as plt
+from matplotlib.collections import LineCollection
+import numpy as np
+
+
+def display_circles(pcs, n_comp, pca, axis_ranks, labels=None, label_rotation=0, lims=None):
+    """
+    Returns pca circles for axis_ranks passed as list of tuples in parameters.
+     Pcs is data, n_comps is needed to error check.
+    Minor clarifications and linting from
+     original function https://github.com/stenier-oc/realisez-une-analyse-de-donnees-exploratoire
+    """
+    for d1, d2 in axis_ranks:  # On affiche les 3 premiers plans factoriels, donc les 6 premières composantes
+        if d2 < n_comp:
+
+            # initialisation de la figure
+            fig, ax = plt.subplots(figsize=(18, 18))
+
+            # détermination des limites du graphique
+            if lims is not None:
+                xmin, xmax, ymin, ymax = lims
+            elif pcs.shape[1] < 30:
+                xmin, xmax, ymin, ymax = -1, 1, -1, 1
+            else:
+                xmin, xmax, ymin, ymax = min(pcs[d1, :]), max(pcs[d1, :]), min(pcs[d2, :]), max(pcs[d2, :])
+
+            # affichage des flèches
+            # s'il y a plus de 30 flèches, on n'affiche pas le triangle à leur extrémité
+            if pcs.shape[1] < 30:
+                plt.quiver(np.zeros(pcs.shape[1]), np.zeros(pcs.shape[1]),
+                           pcs[d1, :], pcs[d2, :],
+                           angles='xy', scale_units='xy', scale=1, color="grey"
+                           )
+
+                # (voir la doc : https://matplotlib.org/api/_as_gen/matplotlib.pyplot.quiver.html)
+            else:
+                lines = [[[0, 0], [x, y]] for x, y in pcs[[d1, d2]].T]
+                ax.add_collection(LineCollection(lines, axes=ax, alpha=.1, color='black'))
+
+            # affichage des noms des variables
+            if labels is not None:
+                for i, (x, y) in enumerate(pcs[[d1, d2]].T):
+                    if x >= xmin and x <= xmax and y >= ymin and y <= ymax:
+                        plt.text(
+                                    x, y, labels[i], fontsize='14', ha='center', va='center',
+                                    rotation=label_rotation, color="blue", alpha=0.5
+                                )
+
+            # affichage du cercle
+            circle = plt.Circle((0, 0), 1, facecolor='none', edgecolor='b')
+            plt.gca().add_artist(circle)
+
+            # définition des limites du graphique
+            plt.xlim(xmin, xmax)
+            plt.ylim(ymin, ymax)
+
+            # affichage des lignes horizontales et verticales
+            plt.plot([-1, 1], [0, 0], color='grey', ls='--')
+            plt.plot([0, 0], [-1, 1], color='grey', ls='--')
+
+            # nom des axes, avec le pourcentage d'inertie expliqué
+            plt.xlabel(f"PC{d1+1} ({round(100*pca.explained_variance_ratio_[d1],1)}%)")
+            plt.ylabel(f"PC{d2+1} ({round(100*pca.explained_variance_ratio_[d2],1)}%)")
+
+            plt.title(f"Cercle des corrélations (PC{d1 + 1} et PC{d2 + 1})")
+            plt.show(block=False)
