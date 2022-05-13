576a72754a51215f089ae3c524eaac7edc1f0e43
Pierre Sempere <pierre.sempere.01@gmail.com>
2022-05-13 11:16:10 +0200

    added export

---
+++ models/ml_alg.py
@@ -0,0 +1,10 @@
+
+class Ml_alg():
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+    def __str__(self) -> str:
+        return self.name
+
+    def __repr__(self) -> str:
+        return self.name
